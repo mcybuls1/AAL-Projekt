44235fac2741bdb48ab386df9af896ffc7c25c66
Mikolaj Cybulski <mikolaj_cybulski@hotmail.com>
2014-04-13 23:10:44 +0200

    timer

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Dobry projekt!" << endl;
+    clock_t czasStart = clock();
+    cout << "Dobry projekt! wykonuje sie:" << endl;
+    for (int i = 0; i < 300000000; i++);
+    float czasWykonania = ((float)clock() - czasStart)/CLOCKS_PER_SEC;
+    cout << "Czas wykonania: " << czasWykonania << " sec"<< endl;
+
     return 0;
 }
 
